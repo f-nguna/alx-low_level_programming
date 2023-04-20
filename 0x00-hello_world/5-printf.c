5372e49305f209be15673a1041b0361458f50097
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-20 11:40:26 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printf the give words
+ * Return: 0 if th code success. otherwiser no success
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
