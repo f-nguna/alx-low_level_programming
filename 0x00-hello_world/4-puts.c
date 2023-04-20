24973459730b285ee58846f5dddbb849dd946092
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-20 11:22:34 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is a main function
+ * thi is a puts dispaly code
+ * return: 0 if the code is all complete
+ * this is to show the code success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
