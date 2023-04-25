0a228e4735044fe384ab8a4d5796466059da07c0
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-24 19:50:16 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Shows the start of the code
+ * Return: 0 if th code success. otherwiser no success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i--)
+		printf("%c", i);
+
+	printf("\n");
+	return (0);
+}
