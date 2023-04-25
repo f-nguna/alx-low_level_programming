86d24db8e77b7a95fd36fddf59fc6b98d98da78f
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-24 19:58:54 -0700

    redoo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Shows the start of the code
+ * Return: 0 if th code success. otherwiser no success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+	for (i = 97; i <= 102; i++)
+		putchar( 'a' + 1 );
+
+	putchar('\n');
+	return (0);
+}
