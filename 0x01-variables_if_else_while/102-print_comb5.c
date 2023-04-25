51f7ee7c3f9cc770d4b262fe7786ad654c42abfd
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-25 05:22:20 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,28 +7,26 @@
 
 int main(void)
 {
-	int i, j, k, l, m;
+	int i, j, l, m;
 	int ch = ',';
 	int sp = ' ';
 
-	for (k = 0; k < 10; k++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
-			for (j = 0; j < 10; j++)
+			for (l = i; l < 10; l++)
 			{
-				for (l = 0; l < 10; l++)
+				m = j + 1;
+				for (m = 0; m < 10; m++)
 				{
-					for (m = 0; m < 10; m++)
-					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(sp);
-						putchar('0' + l);
-						putchar('0' + m);
-						putchar(ch);
-						putchar(sp);
-					}
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(sp);
+					putchar('0' + l);
+					putchar('0' + m);
+					putchar(ch);
+					putchar(sp);
 				}
 			}
 		}
