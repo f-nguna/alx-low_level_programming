4cb06cbc5e9cfee22221d74b6d7cf658396aeb57
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-24 19:30:21 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Shows the start of the code
+ * Return: 0 if th code success. otherwiser no success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
