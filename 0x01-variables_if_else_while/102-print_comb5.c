04d1cabb2ef332130fd915e8e331c5a632664bf5
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-27 00:56:56 -0700

    fff

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Shows the start of the code
+ * Return: 0 if th code success. otherwiser no success
+ */
+
+int main(void)
+{
+	int i, j, k, l, m;
+	int ch = ',';
+	int sp = ' ';
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (l = i; l < 10; l++)
+			{
+				m = 
+				for (m = 0; m < 10; m++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(sp);
+					putchar('0' + l);
+					putchar('0' + m);
+					putchar(ch);
+					putchar(sp);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
