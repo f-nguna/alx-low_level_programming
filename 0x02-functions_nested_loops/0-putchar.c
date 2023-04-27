a32b0414c9fb48df3f1de4c2f3f853c17db76f59
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-27 01:27:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Shows the start of the code
+ * Return: 0 if th code success. otherwiser no success
+ */
+
+int main(void)
+{
+	int ch;
+
+	ch = '_';
+	putchar(ch);
+	ch = 'p';
+	putchar(ch);
+	ch = 'u';
+	putchar(ch);
+	ch = 't';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	ch = 'h';
+	putchar(ch);
+	ch = 'a';
+	putchar(ch);
+	ch = 'r';
+	putchar(ch);
+	ch = '.';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
