7c47a8562cc404315147e70f145e1971d55e5f5d
Faith Nguna Mutinda <durhambree60@gmail.com>
2023-04-27 10:47:17 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - identifies if a character is uppercase or lowercase
+ * Return:return 1 for lowecase otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
